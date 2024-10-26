90418e8e524c9826431bb3a0992b31c4f5192521
Yellu <thishappy5318@gmail.com>
2024-10-26 16:20:40 +0900

    Beta Upload v0.0.2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById("loginForm").addEventListener("submit", function (e) {
+/*document.getElementById("loginForm").addEventListener("submit", function (e) {
   e.preventDefault();
   alert("로그인되었습니다.");
 });
@@ -6,4 +6,4 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
 document.getElementById("registerForm").addEventListener("submit", function (e) {
   e.preventDefault();
   alert("회원가입이 완료되었습니다.");
-});
+});*/
